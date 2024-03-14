667c46fd57e8ccdac6bc7192ee4adf3e8780259e
Dreallidar <Realdreanix@gmail.com>
2024-03-15 00:17:35 +0200

    Task 5 completed

--- src/Main.java
+++ src/Main.java
@@ -46,6 +46,11 @@ public class Main {
         String galas = galas1 + galas2;
         System.out.println( galas);
 
+        System.out.println(" Task 5");
+
+        String pie = "An American in Paris";
+        System.out.println(pie.replace("a","*").replace("A","*"));
+
 
     }
 }
